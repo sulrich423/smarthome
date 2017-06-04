cf4cc9f64dac6d34df44d947ea2a8025617b28a8
Sven Ulrich <sven-ulrich@gmx.net>
2017-06-04 13:49:32 +0200

    Updated Playstation-Activity ID

--- src/main/java/smarthome/service/constants/HarmonyPayloads.java
+++ src/main/java/smarthome/service/constants/HarmonyPayloads.java
@@ -6,6 +6,6 @@ import smarthome.service.payload.HarmonyCommandPayload;
 public class HarmonyPayloads {
 
   public static final HarmonyCommandPayload TV_LIGHT_POWERTOGGLE = new HarmonyCommandPayload("43124579", "PowerToggle");
-  public static final HarmonyActivityOnPayload ACTIVITY_PLAYSTATION = new HarmonyActivityOnPayload("25273971");
+  public static final HarmonyActivityOnPayload ACTIVITY_PLAYSTATION = new HarmonyActivityOnPayload("26678390");
 
 }
